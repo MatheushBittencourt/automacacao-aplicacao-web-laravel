d07fb58ec98c4da5dd9dbba02d43be1d38ff366c
Matheus <matheus.hbittencourt@gmail.com>
2025-02-06 23:38:42 -0300

    Inicializado o projeto v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "digitalocean_droplet" "web" {
   region   = var.region
   size     = var.size
   image    = var.image
-  ssh_keys = ["71:2a:16:72:8d:a9:3a:3e:4f:bc:92:2b:7c:78:cd:b4"]
+  ssh_keys = ["Seu Fingerprint da Digital Ocean"]
 
   monitoring = true
 }
---
+++ terraform/terraform.example  copy.tfvars
@@ -0,0 +1,2 @@
+do_token = "Seu Personal access tokens da Digital Ocean"
+ssh_keys = ["Seu Fingerprint da Digital Ocean"]
